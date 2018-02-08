7f8a9990cb38104b292b341f66f6173e4abb60fa
Oleksii Rvachov <orv@ciklum.com>
2018-02-08 13:43:28 +0200

    starting fixing

--- source/Blockly.Bindings.XamarinIOS/Structs.cs
+++ source/Blockly.Bindings.XamarinIOS/Structs.cs
@@ -2,4 +2,32 @@
 
 namespace Blockly.Bindings.XamarinIOS
 {
+    [Native]
+    public enum BKYBlocklyPanGestureRecognizerTouchState : ulong
+    {
+        Began = 0,
+        Changed,
+        Ended,
+        Cancelled
+    }
+    
+    [Native]
+    public enum BKYCodeGeneratorState : ulong
+    {
+        Initialized = 0,
+        Loading,
+        ReadyForUse,
+        Unusable,
+        GeneratingCode
+    }
+    
+    [Native]
+    public enum BKYConnectionType : ulong
+    {
+        PreviousStatement = 0,
+        NextStatement,
+        InputValue,
+        OutputValue
+    }
+    
 }
---
+++ source/Blockly.Bindings.XamarinIOS/old.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CoreAnimation;
+using CoreGraphics;
+//using Darwin;
+//using Dispatch;
+using Foundation;
+using ObjCRuntime;
+//using ObjectiveC;
+using UIKit;
+using WebKit;
+
+namespace Blockly.Bindings.XamarinIOS
+{
+ 
+
+//    interface IBKYLayoutPopoverDelegate
+//    {
+//        
+//    }
+}
